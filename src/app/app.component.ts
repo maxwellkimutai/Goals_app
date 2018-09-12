b680f567945f6f984dce2e03966037b418fdf33c
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-09-12 12:13:05 +0300

    add features to the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-
+import { Goal } from './goal'
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl:'./app.component.html'
 })
 export class AppComponent {
-  title = 'Goals';
+
 }
